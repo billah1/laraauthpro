8545873aa2ebc2a923a3acb80af41d18ba4641cb
billah1 <billah9578@gmail.com>
2023-01-06 21:51:07 +0600

    Formatting validation errors

---
+++ app/Http/Controllers/CustomRegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterStoreRequest;
+
+class CustomRegisterController extends Controller
+{
+    public function registerFormShow(){
+        return view('custom auth.register');
+    }
+    public function registerUser(RegisterStoreRequest $request){
+        // dd($request->all());
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'phone'=>$request->phone,
+            'password'=>Hash::make($request->password)
+
+        ]);
+        
+    }
+}
